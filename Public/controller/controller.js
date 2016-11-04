825c941f24005c9a86c3ef4a931eb9dbd319942f
Taulant Salihi <taulantsalihi@hotmail.se>
2016-11-04 16:54:54 +0100

    Add files via upload

---
+++ Public/controller/controller.js
@@ -0,0 +1,50 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+ 
+ var refresh = function(){
+	$http.get('/contactlist').success(function(response){
+		console.log('i got it');
+		$scope.contactlist = response;
+		$scope.contact = '';
+	});
+};
+refresh();
+
+$scope.addContact = function(){
+	console.log($scope.contact);
+	$http.post('/contactlist', $scope.contact).success(function(response){
+		console.log(response);
+		refresh();
+	});
+};
+
+$scope.remove = function(id) {
+	console.log(id);
+	$http.delete('/contactlist/' + id).success(function(response){
+		refresh();
+	});
+};
+
+  $scope.edit = function(contact){ 
+   	console.log(contact);
+ $scope.contact = contact;
+};
+
+$scope.update = function(contact) {
+	console.log($scope.contact);
+	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+		refresh();
+	})
+};
+
+$scope.deselect = function(){
+	$scope.contact = '';
+}
+
+
+}]);
+
+
+
+
